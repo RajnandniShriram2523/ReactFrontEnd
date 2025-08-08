45caba2bdb3bdbaf37ff978f6ef647d6a570404c
Priyanka4784 <priyadeshmukh4784@gmail.com>
2025-08-08 15:14:23 +0530

    l

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -4,16 +4,28 @@ import './login.css';
 
 function Login() {
   return (
-    <div className='logincontainer'>
-    <div className="neumorphic-wrapper">
-      <form className="neumorphic-form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-      
+     <div className="login-container">
+      <div className="logo">
+        <img src="https://thumbs.dreamstime.com/b/hand-book-logo-illustration-art-background-43965136.jpg" alt="Book Logo" />
+      </div>
+
+      <div className="login-box">
+        <form>
+          <div className="mb-3 text-start">
+            <label htmlFor="username" className="form-label">Username:</label>
+            <input type="text" className="form-control" id="username" placeholder="Enter username" />
+          </div>
+
+          <div className="mb-3 text-start">
+            <label htmlFor="password" className="form-label">Password:</label>
+            <input type="password" className="form-control" id="password" placeholder="Enter password" />
+          </div>
+
+          <a href='' className="btn login-btn">Login </a>
+        </form>
+      </div>
     </div>
-    </div>
+    
   );
 }
 
