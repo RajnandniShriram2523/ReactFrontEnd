5117382911631f62f92a8a8b05c28bbdda152e1b
RajnandniShriram2523 <rajnandnishriram2505@gmail.com>
2025-08-16 09:54:33 +0530

    ch

--- src/slidelingimg.jsx
+++ src/slidelingimg.jsx
@@ -7,30 +7,22 @@ export default class SlidelingImg extends React.Component {
     render() {
         return <>
             <div className="wall">
-               <div className="headling">
-  <h1 className="typewriter">
-    Welcome to Library priyanka rajnandni
-    jhgf hjgsdf hjdfgjh hjgruy3 hevryuiwr
-  </h1>
-</div>
-
-
-
-
-
-
-                {<div class="slide-track">
-                    <div class="slide"><img src="https://www.shutterstock.com/image-photo/caucasian-female-student-stands-university-260nw-1901439172.jpg" alt="1" /></div>
-                    <div class="slide"><img src="https://i.ytimg.com/vi/-2XJP7JGcuM/mqdefault.jpg" alt="2" /></div>
-                    <div class="slide"><img src="https://www.shutterstock.com/image-photo/woman-model-college-student-working-260nw-1197146626.jpg" alt="3" /></div>
-                    <div class="slide"><img src="https://www.bournemouth.ac.uk/sites/default/files/styles/b_4_3_desktop/public/assets/images/books-library-summary.jpg?itok=U2Zfe9Ue" alt="4" /></div>
-                    <div class="slide"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1XsVt2HTxzDWxidSUNLBAa7Whiumu7MLoAoccszExY2ZvHUGhv-SScRleeXKjQTLLb9I&usqp=CAU" alt="5" /></div>
-
-                    <div class="slide"><img src="https://www.shutterstock.com/image-photo/caucasian-female-student-stands-university-260nw-1901439172.jpg" alt="1" /></div>
-                    <div class="slide"><img src="https://i.ytimg.com/vi/-2XJP7JGcuM/mqdefault.jpg" alt="2" /></div>
-                    <div class="slide"><img src="https://www.shutterstock.com/image-photo/woman-model-college-student-working-260nw-1197146626.jpg" alt="3" /></div>
-                    <div class="slide"><img src="https://www.bournemouth.ac.uk/sites/default/files/styles/b_4_3_desktop/public/assets/images/books-library-summary.jpg?itok=U2Zfe9Ue" alt="4" /></div>
-                    <div class="slide"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1XsVt2HTxzDWxidSUNLBAa7Whiumu7MLoAoccszExY2ZvHUGhv-SScRleeXKjQTLLb9I&usqp=CAU" alt="5" /></div>
+                <div className="headling">
+                    { <h1 className="typewriter">
+    Welcome to Library priyanka rajnandni jhgf hjgsdf hjdfgjh hjgruy3 hevryuiwr
+  </h1> }
+                </div>
+                {
+                    <div class="slide-track">
+                    <div class="slide"><img src="public/Image/bg1.jpg" alt="1" /></div>
+                    <div class="slide"><img src="public/Image/bg4.jpg" alt="1" /></div>
+                    <div class="slide"><img src="public/Image/bg2.jpg" alt="1" /></div>
+                    <div class="slide"><img src="public/Image/bg5.jpg" alt="1" /></div>
+                    <div class="slide"><img src="public/Image/bg3.jpg" alt="1" /></div>
+                    <div class="slide"><img src="public/Image/bg8.jpg" alt="1" /></div>
+                    <div class="slide"><img src="public/Image/bg7.jpg" alt="2" /></div>
+                    <div class="slide"><img src="public/Image/bg1.jpg" alt="2" /></div>
+                    <div class="slide"><img src="public/Image/bg9.jpg" alt="2" /></div>
 
                 </div>}
 
